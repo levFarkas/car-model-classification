4ef01aeb640325492832e765a0603e8aa173308a
leventefarkas <levnzz@gmail.com>
2023-04-24 16:14:47 +0200

    feat: add sagemaker

--- main.tf
+++ main.tf
@@ -1,3 +1,218 @@
-resource "aws_s3_bucket" "cmc_train_data_bucket" {
-  bucket = "car-model-classification-images"
+data "aws_region" "current" {}
+
+locals {
+  input_training_path = "s3://${var.s3_bucket_input_training_path}"
+  output_models_path  = "s3://${var.s3_bucket_output_models_path}"
 }
+
+#################################################
+# IAM Roles and Policies for Step Functions
+#################################################
+
+// IAM role for Step Functions state machine
+data "aws_iam_policy_document" "sf_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["states.${data.aws_region.current.name}.amazonaws.com"]
+    }
+  }
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "sf_exec_role" {
+  name               = "${var.project_name}-sfn-exec"
+  assume_role_policy = data.aws_iam_policy_document.sf_assume_role.json
+}
+
+resource "aws_iam_policy" "lambda_invoke" {
+  name   = "${var.project_name}-lambda-invoke"
+  policy = data.aws_iam_policy_document.lambda_invoke.json
+}
+
+data "aws_iam_policy_document" "lambda_invoke" {
+  statement {
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    resources = [
+      aws_lambda_function.lambda_function.arn,
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_invoke" {
+  role       = aws_iam_role.sf_exec_role.name
+  policy_arn = aws_iam_policy.lambda_invoke.arn
+}
+
+// policy to invoke sagemaker training job, creating endpoints etc.
+resource "aws_iam_policy" "sagemaker_policy" {
+  name   = "${var.project_name}-sagemaker"
+  policy = <<-EOF
+      {
+          "Version": "2012-10-17",
+          "Statement": [
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "sagemaker:CreateTrainingJob",
+                      "sagemaker:DescribeTrainingJob",
+                      "sagemaker:StopTrainingJob",
+                      "sagemaker:createModel",
+                      "sagemaker:createEndpointConfig",
+                      "sagemaker:createEndpoint",
+                      "sagemaker:addTags"
+                  ],
+                  "Resource": [
+                   "*"
+                  ]
+              },
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "sagemaker:ListTags"
+                  ],
+                  "Resource": [
+                   "*"
+                  ]
+              },
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "iam:PassRole"
+                  ],
+                  "Resource": [
+                   "*"
+                  ],
+                  "Condition": {
+                      "StringEquals": {
+                          "iam:PassedToService": "sagemaker.amazonaws.com"
+                      }
+                  }
+              },
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "events:PutTargets",
+                      "events:PutRule",
+                      "events:DescribeRule"
+                  ],
+                  "Resource": [
+                  "*"
+                  ]
+              }
+          ]
+      }
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "sm_invoke" {
+  role       = aws_iam_role.sf_exec_role.name
+  policy_arn = aws_iam_policy.sagemaker_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "cloud_watch_full_access" {
+  role       = aws_iam_role.sf_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+#################################################
+# IAM Roles and Policies for SageMaker
+#################################################
+
+// IAM role for SageMaker training job
+data "aws_iam_policy_document" "sagemaker_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["sagemaker.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "sagemaker_exec_role" {
+  name               = "${var.project_name}-sagemaker-exec"
+  assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
+}
+
+// Policies for sagemaker execution training job
+resource "aws_iam_policy" "sagemaker_s3_policy" {
+  name   = "${var.project_name}-sagemaker-s3-policy"
+  policy = <<-EOF
+      {
+          "Version": "2012-10-17",
+          "Statement": [
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "s3:*"
+                  ],
+                  "Resource": [
+                   "${aws_s3_bucket.bucket_training_data.arn}",
+                   "${aws_s3_bucket.bucket_output_models.arn}",
+                   "${aws_s3_bucket.bucket_training_data.arn}/*",
+                   "${aws_s3_bucket.bucket_output_models.arn}/*"
+                  ]
+              }
+          ]
+      }
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "s3_restricted_access" {
+  role       = aws_iam_role.sagemaker_exec_role.name
+  policy_arn = aws_iam_policy.sagemaker_s3_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker_full_access" {
+  role       = aws_iam_role.sagemaker_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
+
+#################################################
+# ECR Repository
+#################################################
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = var.project_name
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+#################################################
+# S3 Buckets
+#################################################
+
+resource "aws_s3_bucket" "cmc_train_data_bucket" {
+  bucket = var.s3_bucket_input_training_path
+}
+
+resource "aws_s3_bucket_acl" "cmc_train_data_bucket_acl" {
+  bucket = aws_s3_bucket.bucket_training_data.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "cmc_output_models_bucket" {
+  bucket = var.s3_bucket_output_models_path
+}
+
+resource "aws_s3_bucket_acl" "cmc_output_models_bucket_acl" {
+  bucket = aws_s3_bucket.bucket_output_models.id
+  acl    = "private"
+}
+
