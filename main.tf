c0ed25846bdf41770e4f419ea5d489de2ea5a77b
leventefarkas <levnzz@gmail.com>
2023-05-20 18:33:55 +0200

    feat: added step function and lambda

--- main.tf
+++ main.tf
@@ -195,3 +195,120 @@ resource "aws_s3_bucket_acl" "cmc_output_models_bucket_acl" {
   acl    = "private"
 }
 
+#################################################
+# Step Function
+#################################################
+
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "${var.project_name}-state-machine"
+  role_arn = aws_iam_role.sf_exec_role.arn
+
+  definition = <<-EOF
+  {
+  "Comment": "An AWS Step Function State Machine to train, build and deploy an Amazon SageMaker model endpoint",
+  "StartAt": "Configuration Lambda",
+  "States": {
+    "Configuration Lambda": {
+      "Type": "Task",
+      "Resource": "${aws_lambda_function.lambda_function.arn}",
+      "Parameters": {
+        "PrefixName": "${var.project_name}",
+        "input_training_path": "$.input_training_path"
+        },
+      "Next": "Create Training Job",
+      "ResultPath": "$.training_job_name"
+      },
+    "Create Training Job": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::sagemaker:createTrainingJob.sync",
+      "Parameters": {
+        "TrainingJobName.$": "$.training_job_name",
+        "ResourceConfig": {
+          "InstanceCount": 1,
+          "InstanceType": "${var.training_instance_type}",
+          "VolumeSizeInGB": ${var.volume_size_sagemaker}
+        },
+        "HyperParameters": {
+          "test": "test"
+        },
+        "AlgorithmSpecification": {
+          "TrainingImage": "${aws_ecr_repository.ecr_repository.repository_url}",
+          "TrainingInputMode": "File"
+        },
+        "OutputDataConfig": {
+          "S3OutputPath": "s3://${aws_s3_bucket.bucket_output_models.bucket}"
+        },
+        "StoppingCondition": {
+          "MaxRuntimeInSeconds": 86400
+        },
+        "RoleArn": "${aws_iam_role.sagemaker_exec_role.arn}",
+        "InputDataConfig": [
+        {
+          "ChannelName": "training",
+          "ContentType": "text/csv",
+          "DataSource": {
+            "S3DataSource": {
+              "S3DataType": "S3Prefix",
+              "S3Uri": "s3://${aws_s3_bucket.bucket_training_data.bucket}",
+              "S3DataDistributionType": "FullyReplicated"
+            }
+          }
+        }
+        ]
+      },
+      "Next": "Create Model"
+    },
+    "Create Model": {
+      "Parameters": {
+        "PrimaryContainer": {
+          "Image": "${aws_ecr_repository.ecr_repository.repository_url}",
+          "Environment": {},
+          "ModelDataUrl.$": "$.ModelArtifacts.S3ModelArtifacts"
+        },
+        "ExecutionRoleArn": "${aws_iam_role.sagemaker_exec_role.arn}",
+        "ModelName.$": "$.TrainingJobName"
+      },
+      "Resource": "arn:aws:states:::sagemaker:createModel",
+      "Type": "Task",
+      "ResultPath":"$.taskresult",
+      "Next": "Create Endpoint Config"
+    },
+    "Create Endpoint Config": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::sagemaker:createEndpointConfig",
+      "Parameters":{
+        "EndpointConfigName.$": "$.TrainingJobName",
+        "ProductionVariants": [
+        {
+          "InitialInstanceCount": 1,
+          "InstanceType": "${var.inference_instance_type}",
+          "ModelName.$": "$.TrainingJobName",
+          "VariantName": "AllTraffic"
+        }
+        ]
+      },
+      "ResultPath":"$.taskresult",
+      "Next":"Create Endpoint"
+    },
+    "Create Endpoint":{
+      "Type":"Task",
+      "Resource":"arn:aws:states:::sagemaker:createEndpoint",
+      "Parameters":{
+        "EndpointConfigName.$": "$.TrainingJobName",
+        "EndpointName.$": "$.TrainingJobName"
+      },
+      "End": true
+      }
+    }
+  }
+  EOF
+}
+
+#################################################
+# Outputs
+#################################################
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.ecr_repository.repository_url
+  description = "ECR URL for the Docker Image"
+}
